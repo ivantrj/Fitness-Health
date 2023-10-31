6887b6c10587cd2a552eb9798858a8818f4fa00a
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-10-31 10:55:33 +0100

    first commit

---
+++ Fitness&Health/Fitness_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_HealthApp.swift
+//  Fitness&Health
+//
+//  Created by ivan ruwido  on 31.10.23.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
